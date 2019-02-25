bd5eacb4123549b1ec978812866f91b84b3bf916
Felix Kunz <felix.kunzjr@gmail.com>
2019-02-25 17:15:35 +0100

    Update SerialMega-ZIP30.ino

--- SerialMega-ZIP30/SerialMega-ZIP30.ino
+++ SerialMega-ZIP30/SerialMega-ZIP30.ino
@@ -34,7 +34,7 @@ void setup() {
   Serial.begin(9600);
   Serial1.begin(1200, SERIAL_7E1);
   MaxChar = 90;
-  CRLF();
+  CRLF();                               //Carriage Return / Line Feed
   Store();
 
   pinMode(52, INPUT);                   //If Jumper set, ^ will be printed instead of initiate CRLF
